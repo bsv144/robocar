563e56dad81a66e9d37d2bc3ae03019e8137aad1
Sergey <bsv_144@mail.ru>
2015-11-19 06:50:46 +0500

    Add struct MOTOR

--- robocar.ino
+++ robocar.ino
@@ -1,3 +1,18 @@
+/*
+ * Управление двигателями через драйвер L293
+ *
+ *
+*/
+
+
+struct MOTOR
+{
+  int in1;
+  int in2;
+  int enable;
+}
+
+
 void setup() {
   // put your setup code here, to run once:
 
